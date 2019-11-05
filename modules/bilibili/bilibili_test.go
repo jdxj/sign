d22ba1fd3c5cf8c3fcb9c8787f071b7f71a92960
wangyouqiang <985759262@qq.com>
2019-11-05 11:12:38 +0800

    refactor: 重构配置文件读取

--- modules/bilibili/bilibili_test.go
+++ modules/bilibili/bilibili_test.go
@@ -9,7 +9,7 @@ import (
 
 func newTouchBilibili() (*TouchBilibili, error) {
 	t := &TouchBilibili{
-		cookies:     "_uuid=C52E52BE-5D02-D899-B8A1-6EA59C48E68537621infoc; buvid3=760E75A7-3BE5-466C-9D36-D105DEDA550D190946infoc; LIVE_BUVID=AUTO4515713108385339; sid=89mn1hef; DedeUserID=98634211; DedeUserID__ckMd5=70cb5476ec3f0977; SESSDATA=aab9cad1%2C1573902850%2C06835ba1; bili_jct=d6c6abce34fcf6c6bcd4be93ce0dc455; bp_t_offset_98634211=318145776854474289",
+		cookies:     "",
 		loginURL:    "https://space.bilibili.com/98634211",
 		verifyKey:   "title",
 		verifyValue: "王者王尼玛的个人空间 - 哔哩哔哩 ( ゜- ゜)つロ 乾杯~ Bilibili",
--- utils/conf/conf.go
+++ utils/conf/conf.go
@@ -1,1 +1,16 @@
 package conf
+
+import "gopkg.in/ini.v1"
+
+var (
+	Conf *ini.File
+)
+
+func init() {
+	conf, err := ini.Load("sign.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	Conf = conf
+}
