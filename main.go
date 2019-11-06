0f8719c8756c0f2cfd1185e91edec82673cfcae2
wangyouqiang <985759262@qq.com>
2019-11-06 09:33:26 +0800

    fix: 修复邮件格式的错误

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	err := email.SendEmail("sing start", "please notice log")
+	err := email.SendEmail("sing start", "please notice %s", "log file")
 	if err != nil {
 		log.MyLogger.Warn("%s %s", log.Log_Main, err)
 	}
