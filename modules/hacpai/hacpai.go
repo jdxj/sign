9588599f1d2c7d0f72d0d68d49287b50c0dbc2de
wangyouqiang <985759262@qq.com>
2019-11-08 11:46:55 +0800

    feat: 完成 hacpai 的签到

--- modules/hacpai/hacpai.go
+++ modules/hacpai/hacpai.go
@@ -1,17 +1,15 @@
 package hacpai
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"gopkg.in/ini.v1"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
-	"net/url"
-	"sign/utils"
 	"sign/utils/log"
-
-	"github.com/PuerkitoBio/goquery"
-
-	"gopkg.in/ini.v1"
 )
 
 func NewToucherHacPai(sec *ini.Section) (*ToucherHacPai, error) {
@@ -20,14 +18,12 @@ func NewToucherHacPai(sec *ini.Section) (*ToucherHacPai, error) {
 	}
 
 	tou := &ToucherHacPai{
-		//username: sec.Key("username").String(),
-		//password: sec.Key("password").String(),
-		cookies:     sec.Key("cookies").String(),
-		loginURL:    sec.Key("loginURL").String(),
-		signURL:     sec.Key("signURL").String(),
-		verifyKey:   sec.Key("verifyKey").String(),
-		verifyValue: sec.Key("verifyValue").String(),
-		client:      &http.Client{},
+		username:   sec.Key("username").String(),
+		password:   sec.Key("password").String(),
+		loginURL:   "https://hacpai.com/api/v2/login",
+		signRefURL: "https://hacpai.com/activity/checkin",
+		signURL:    "https://hacpai.com/activity/daily-checkin",
+		client:     &http.Client{},
 	}
 
 	jar, err := cookiejar.New(nil)
@@ -40,15 +36,13 @@ func NewToucherHacPai(sec *ini.Section) (*ToucherHacPai, error) {
 }
 
 type ToucherHacPai struct {
-	//username string
-	//password string
-	cookies string
+	username string
+	password string // 需要 md5 值
+	token    string
 
-	loginURL string
-	signURL  string
-
-	verifyKey   string
-	verifyValue string
+	loginURL   string
+	signRefURL string
+	signURL    string
 
 	client *http.Client
 }
@@ -58,26 +52,77 @@ func (tou *ToucherHacPai) Name() string {
 }
 
 func (tou *ToucherHacPai) Boot() bool {
-	cookies, err := utils.StrToCookies(tou.cookies, utils.HacPaiCookieDomain)
-	if err != nil {
-		log.MyLogger.Error("%s %s", log.Log_HacPai, err)
-		return false
-	}
-
-	cookieURL, err := url.Parse(utils.HacPaiCookieURL)
-	if err != nil {
-		log.MyLogger.Error("%s %s", log.Log_HacPai, err)
-		return false
-	}
-
-	tou.client.Jar.SetCookies(cookieURL, cookies)
 	return true
 }
 
 func (tou *ToucherHacPai) Login() bool {
-	resp, err := tou.client.Get(tou.loginURL)
+	loginData := make(map[string]interface{})
+	loginData["userName"] = tou.username
+	loginData["userPassword"] = tou.password
+	loginData["captcha"] = ""
+	loginDataJson, err := json.Marshal(loginData)
+	if err != nil {
+		log.MyLogger.Error("%s marshal login data err: %s", log.Log_HacPai, err)
+		return false
+	}
+	reader := bytes.NewReader(loginDataJson)
+	req, err := http.NewRequest("POST", tou.loginURL, reader)
+	if err != nil {
+		log.MyLogger.Error("%s new login req err: %s", log.Log_HacPai, err)
+		return false
+	}
+	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36")
+	resp, err := tou.client.Do(req)
+	if err != nil {
+		log.MyLogger.Error("%s err when send login data: %s", log.Log_HacPai, err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.MyLogger.Error("%s err when read login resp: %s", log.Log_HacPai, err)
+		return false
+	}
+
+	respDate := make(map[string]interface{})
+	if err = json.Unmarshal(respBytes, &respDate); err != nil {
+		log.MyLogger.Error("%s err when unmarshal login resp: %s", log.Log_HacPai, err)
+		return false
+	}
+
+	if tmp, ok := respDate["token"]; ok {
+		if token, mark := tmp.(string); mark && token != "" {
+			tou.token = token
+			return mark
+		}
+	}
+
+	log.MyLogger.Debug("%s login token not found", log.Log_HacPai)
+	return false
+}
+
+func (tou *ToucherHacPai) Sign() bool {
+	req, err := http.NewRequest("GET", tou.signURL, nil)
 	if err != nil {
 		log.MyLogger.Error("%s %s", log.Log_HacPai, err)
+		return false
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36")
+	req.Header.Set("Referer", tou.signRefURL)
+	cookie := http.Cookie{
+		Name:   "symphony",
+		Value:  tou.token,
+		Path:   "/",
+		MaxAge: 86400,
+	}
+	req.AddCookie(&cookie)
+
+	resp, err := tou.client.Do(req)
+	if err != nil {
+		log.MyLogger.Error("%s err when send sign req: %s", log.Log_HacPai, err)
 		return false
 	}
 	defer resp.Body.Close()
@@ -88,54 +133,12 @@ func (tou *ToucherHacPai) Login() bool {
 		return false
 	}
 
-	var mark bool
-	doc.Find(tou.verifyKey).Each(func(i int, selection *goquery.Selection) {
-		fmt.Println("selection:", selection.Text())
-		if selection.Text() == tou.verifyValue {
-			mark = true
-		}
-	})
-	return mark
-}
-
-// todo: 实现
-func (tou *ToucherHacPai) Sign() bool {
-	resp, err := tou.client.Get(tou.signURL)
-	if err != nil {
-		log.MyLogger.Error("%s %s", log.Log_HacPai, err)
+	target := doc.Find(".module__body").Find(".btn")
+	if target == nil {
+		log.MyLogger.Error("%s score not found, web page maybe change", log.Log_HacPai)
 		return false
+	} else {
+		log.MyLogger.Info("%s score is: %s", log.Log_HacPai, target.Text())
+		return true
 	}
-	defer resp.Body.Close()
-
-	//doc, err := goquery.NewDocumentFromReader(resp.Body)
-	//if err != nil {
-	//	log.MyLogger.Error("%s %s", log.Log_HacPai, err)
-	//	return false
-	//}
-	//
-	//realSignURL, ok := doc.Find(".green").Attr("href")
-	//if !ok {
-	//	log.MyLogger.Error("%s %s", log.Log_HacPai, "real sign url not found")
-	//	return false
-	//}
-	//fmt.Println("real sign url:", realSignURL)
-	//
-	//// todo: 假设 get 方法可以访问
-	//resp, err = tou.client.Get(realSignURL)
-	//if err != nil {
-	//	log.MyLogger.Error("%s can not access real sign url: %s, %s", log.Log_HacPai, realSignURL, err)
-	//	return false
-	//}
-	defer resp.Body.Close()
-
-	//var mark bool
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.MyLogger.Error("%s can not read resp body after get real sign url: %s", log.Log_HacPai, err)
-		return false
-	}
-
-	fmt.Printf("%s\n", data)
-
-	return false
 }
