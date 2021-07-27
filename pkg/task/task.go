e35866d445712958caf8169201d09a6f83748af3
jdxj <985759262@qq.com>
2021-07-27 18:21:54 +0800

    refactor: 调度时间

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -3,25 +3,15 @@ package task
 import (
 	"github.com/robfig/cron/v3"
 
-	"github.com/jdxj/sign/internal/logger"
 	"github.com/jdxj/sign/pkg/task/bili"
+	"github.com/jdxj/sign/pkg/task/hpi"
 )
 
 func Run() {
 	c := cron.New()
-	//spec := "* * * * *"
-	//spec := "0 8 * * *"
-	_, err := c.AddFunc("0 8 * * *", bili.RunSignTask)
-	if err != nil {
-		logger.Errorf("add func: %s", err)
-		return
-	}
+	_, _ = c.AddFunc("0 20 * * *", bili.RunSignTask)
+	_, _ = c.AddFunc("1 20 * * *", bili.RunBCountTask)
 
-	_, err = c.AddFunc("0 20 * * *", bili.RunBCountTask)
-	if err != nil {
-		logger.Errorf("add func: %s", err)
-		return
-	}
-
+	_, _ = c.AddFunc("0 20 * * *", hpi.RunSignTask)
 	c.Run()
 }
