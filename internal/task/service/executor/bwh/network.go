e7cb09cbe2ff67b4c6d54ca2d24600c1c0c602d0
王友强 <985759262@qq.com>
2022-02-08 00:23:50 +0800

    feat: stat for the month

--- internal/task/service/executor/bwh/network.go
+++ internal/task/service/executor/bwh/network.go
@@ -2,6 +2,7 @@ package bwh
 
 import (
 	"fmt"
+	"time"
 
 	kwv "github.com/jdxj/kiwivm-sdk-go"
 	"google.golang.org/protobuf/proto"
@@ -14,6 +15,11 @@ const (
 	msgGetNetworkFailed = "获取流量使用数据失败"
 	msgGetCPUFailed     = "获取CPU使用数据失败"
 	msgCPULimit         = "CPU使用可能超过100%"
+)
+
+const (
+	// Byte to Gigabyte
+	gb float64 = 1 << 30
 )
 
 type NetworkMeter struct {
@@ -36,11 +42,32 @@ func (nm *NetworkMeter) Execute(body []byte) (string, error) {
 		return msgGetNetworkFailed, err
 	}
 
-	var count int64
-	// 一天有288个5分钟
-	for i := len(rsp.Data) - 1; i >= 0 && i >= len(rsp.Data)-288; i-- {
-		count += rsp.Data[i].NetworkInBytes + rsp.Data[i].NetworkOutBytes
+	var (
+		now   = time.Now()
+		day   = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+		month = time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.Local)
+
+		dayCount   int64
+		monthCount int64
+	)
+	for i := len(rsp.Data) - 1; i >= 0; i-- {
+		v := rsp.Data[i]
+		t := time.Unix(v.Timestamp, 0)
+
+		if t.After(day) {
+			dayCount += v.NetworkInBytes + v.NetworkOutBytes
+		}
+		if t.After(month) {
+			monthCount += v.NetworkInBytes + v.NetworkOutBytes
+		} else {
+			// rsp.Data 是按照时间排序的,
+			// 一个月的数据查完后直接退出.
+			break
+		}
 	}
-	return fmt.Sprintf("BWH流量使用: %.3fMB",
-		float64(count)/float64(1000*1000)), nil
+	return fmt.Sprintf(`[BWH流量使用]
+当天用量: %.3fGB
+当月用量: %.3fGB`,
+		float64(dayCount)/gb,
+		float64(monthCount)/gb), nil
 }
