6adda15e8ed450d4c4b8cf187d4793f10584b2fc
jdxj <985759262@qq.com>
2022-02-11 09:36:23 +0800

    test

--- internal/app/router/v1/v1.go
+++ internal/app/router/v1/v1.go
@@ -2,31 +2,27 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
-
-	"github.com/jdxj/sign/internal/app/handler"
 )
 
 func New(p gin.IRouter) {
 	v1 := p.Group("/v1")
-	v1.GET("/", handler.Hello)
 
-	v1.POST("/login", handler.Login)
-	v1.POST("/users", handler.SignUp)
+	v1.POST("/login", Login)
+	v1.POST("/users", SignUp)
 
 	// user
 	users := v1.Group("/users", authnBearer)
 	{
-		users.PUT("/:user_id", handler.UpdateUser)
+		users.PUT("/:user_id", UpdateUser)
 	}
 
 	// task
 	tasks := v1.Group("/tasks", authnBearer)
 	{
-		tasks.POST("", handler.CreateTask)
-		tasks.GET("", handler.GetTasks)
-		tasks.GET("/:task_id", handler.GetTask)
-		tasks.PUT("/:task_id", handler.UpdateTask)
-		tasks.DELETE("/:task_id", handler.DeleteTask)
+		tasks.POST("", CreateTask)
+		tasks.GET("", GetTasks)
+		tasks.GET("/:task_id", GetTask)
+		tasks.PUT("/:task_id", UpdateTask)
+		tasks.DELETE("/:task_id", DeleteTask)
 	}
-
 }
