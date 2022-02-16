ca04cdb6d9e3a56c98c0211918bebf9b0ab43c9a
jdxj <985759262@qq.com>
2022-02-17 00:46:25 +0800

    feat(trigger): add param for @randomly

--- internal/trigger/service/cron.go
+++ internal/trigger/service/cron.go
@@ -17,48 +17,52 @@ const (
 	//   '@randomly 8:00:00 23:00:00' means 8:00:00~23:00:00
 	randomly     = "@randomly"
 	randomLayout = "15:04:05"
+
+	randomStart = "00:00:00"
+	randomEnd   = "23:59:59"
 )
 
 var (
 	ErrInvalidRandomly = errors.New("invalid randomly expression")
+
+	randomStartTime, _ = time.ParseInLocation(randomLayout, randomStart, time.UTC)
 )
+
+func parseRanges(ts []string) ([]time.Time, error) {
+	result := make([]time.Time, 0, len(ts))
+	for _, v := range ts {
+		if v == "" {
+			result = append(result, randomStartTime)
+			continue
+		}
+
+		t, err := time.ParseInLocation(randomLayout, v, time.UTC)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, t)
+	}
+	return result, nil
+}
 
 func newRandomSchedule(spec string) (*randomSchedule, error) {
 	spec = strings.TrimSpace(strings.TrimPrefix(spec, randomly))
+	ranges := strings.Split(spec, " ")
 
-	var (
-		ranges = strings.Split(spec, " ")
-		start  time.Time
-		end    time.Time
-		err    error
-	)
-
-	switch len(ranges) {
-	case 1:
-		if ranges[0] == "" {
-			start = time.Date(0, 0, 0, 0, 0, 0, 0, time.Local)
-		}
-		if start, err = time.ParseInLocation(randomLayout, ranges[0], time.Local); err != nil {
-			return nil, err
-		}
-		end = time.Date(0, 0, 0, 23, 59, 59, 0, time.Local)
-	case 2:
-		start, err = time.ParseInLocation(randomLayout, ranges[0], time.Local)
-		if err != nil {
-			return nil, err
-		}
-		end, err = time.ParseInLocation(randomLayout, ranges[1], time.Local)
-		if err != nil {
-			return nil, err
-		}
-	default:
+	if len(ranges) == 1 {
+		ranges = append(ranges, randomEnd)
+	} else if len(ranges) > 2 {
 		return nil, ErrInvalidRandomly
 	}
+	result, err := parseRanges(ranges)
+	if err != nil {
+		return nil, err
+	}
 
+	start, end := result[0], result[1]
 	if start.After(end) {
 		return nil, ErrInvalidRandomly
 	}
-
 	return &randomSchedule{
 		start: start,
 		end:   end,
@@ -70,21 +74,26 @@ type randomSchedule struct {
 	end   time.Time
 }
 
+func (rs *randomSchedule) offset() time.Duration {
+	d1 := rs.end.Sub(rs.start)
+	if d1.Nanoseconds() == 0 {
+		return 0
+	}
+	d2 := time.Duration(rand.Int63n(d1.Nanoseconds()))
+	d3 := rs.start.Sub(randomStartTime)
+	return d2 + d3
+}
+
 // Next 返回明天的某个时间
 func (rs *randomSchedule) Next(t time.Time) time.Time {
 	var (
 		year  = t.Year()
 		month = t.Month()
 		day   = t.Day()
-
-		// 留5分钟执行任务, 避免延迟导致当天未执行.
-		bound  = 24*60*60 - 5*60
-		offset = rand.Intn(bound)
 	)
-	rs.end.Sub(rs.start).Truncate()
 	return time.Date(year, month, day, 0, 0, 0, 0, t.Location()).
 		AddDate(0, 0, 1).
-		Add(time.Duration(offset) * time.Second)
+		Add(rs.offset())
 }
 
 func (s *Service) parse(spec string) (cron.Schedule, error) {
--- internal/trigger/service/cron_test.go
+++ internal/trigger/service/cron_test.go
@@ -8,9 +8,23 @@ import (
 )
 
 func TestRandomSchedule_Next(t *testing.T) {
-	rs := &randomSchedule{}
-	for i := 0; i < 100; i++ {
-		fmt.Printf("%s\n", rs.Next(time.Now()))
+	rs, err := newRandomSchedule("")
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
+	for i := 0; i < 10; i++ {
+		tt := rs.Next(time.Now())
+		fmt.Printf("%s\n", tt)
+	}
+
+	fmt.Println()
+	rs, err = newRandomSchedule("@randomly 9:00:00 21:00:01")
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
+	for i := 0; i < 10; i++ {
+		tt := rs.Next(time.Now())
+		fmt.Printf("%s\n", tt)
 	}
 }
 
@@ -20,4 +34,38 @@ func TestSplit(t *testing.T) {
 	for _, v := range res {
 		fmt.Printf("%s\n", v)
 	}
+}
+
+func TestSub(t *testing.T) {
+	t1 := time.Time{}
+	t2 := time.Time{}
+	fmt.Printf("%f\n", t1.Sub(t2).Seconds())
+
+	fmt.Printf("h: %d\n", t1.Hour())
+	fmt.Printf("m: %d\n", t1.Minute())
+	fmt.Printf("s: %d\n", t1.Second())
+	fmt.Printf("t3: %s\n", t1.Add(24*time.Hour))
+	fmt.Printf("t4: %s\n", t1.Add(24*time.Hour-time.Second))
+	fmt.Printf("t5: %s\n", t1.In(time.Local))
+}
+
+func TestParseRanges(t *testing.T) {
+	ts := []string{
+		//"20:60",
+		"20:59:00",
+		"8:59:00",
+	}
+	res, err := parseRanges(ts)
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
+	fmt.Printf("%v\n", res)
+}
+
+func TestRandomInt(t *testing.T) {
+	zero, err := time.ParseInLocation(randomLayout, "00:00:00", time.UTC)
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
+	fmt.Printf("%s\n", zero)
 }
