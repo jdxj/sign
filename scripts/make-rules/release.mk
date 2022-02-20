3360c32c99a8dc567f0fb41c94333d9ea1c99771
jdxj <985759262@qq.com>
2022-02-20 13:45:35 +0800

    build(make)

--- scripts/make-rules/release.mk
+++ scripts/make-rules/release.mk
@@ -6,9 +6,10 @@ release.ensure-tag:
 
 .PHONY: release.tag
 release.tag: release.ensure-tag
-	@git push origin $(shell git describe --tags --abbrev=0)
+	@git push origin $(shell git describe --tags --abbrev=0 --match 'v*')
 
 .PHONY: release.chglog
 release.chglog: release.ensure-tag
-	@git-chglog -o $(CHANGELOG_DIR)/CHANGELOG-$(shell git describe --tags --abbrev=0 --match 'v*' | sed 's/v//g').md \
-	$(shell git describe --tags --abbrev=0 --match 'v*')
+	$(eval VERSION := $(shell git describe --tags --abbrev=0 --match 'v*'))
+	$(eval VERSION_NUM := $(shell echo $(VERSION) | sed 's/v//g'))
+	@git-chglog -o $(CHANGELOG_DIR)/CHANGELOG-$(VERSION_NUM).md $(VERSION)
